daea8d54e5ad89c7cec0f6db6cf0c43e4d789207
Jamsly MINÉ <jmine@haitipay.ht>
2023-07-23 18:31:45 -0400

    add directory and category-item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Directory from "./components/directory/directory.component"
+
+import categories from './categories.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Directory categories={categories}/>
 }
 
-export default App;
+export default App
