1415938c57e7b06805ea6e317948d568634b702f
Jamsly MINÉ <mjamsly98@gmail.com>
2023-07-23 18:40:42 -0400

    Feature/add directory and category (#1)
    * Initialize project using Create React App
    
    * add directory and category-item
    
    ---------
    
    Co-authored-by: Jamsly MINÉ <jmine@haitipay.ht>

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import Directory from "./components/directory/directory.component"
+
+import categories from './categories.json'
+
+function App() {
+  return <Directory categories={categories}/>
+}
+
+export default App
