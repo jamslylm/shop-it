72e68bb3a884a610e39d1a6818db6ff857bdcbac
Jamsly MINÉ <mjamsly98@gmail.com>
2023-10-07 01:59:30 -0400

    Feature/signup with email password (#3)
    * feat: added sign in component
    
    Added the sign in component as well as firebase authentication and setup firestore for data storing
    
    * created sign up component, the form input component and the button component
    
    ---------
    
    Co-authored-by: Jamsly MINÉ <mjamsly98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css'
-import Directory from "./components/directory/directory.component"
+import {Routes, Route} from "react-router-dom"
 
-import categories from './categories.json'
+import Home from "./routes/home/home.component"
+import Navigation from "./routes/navigation/navigation.component"
+import SignIn from "./routes/sign-in/sign-in.component"
+
+const Shop = () => {
+  return <h1>Shop</h1>
+}
 
 function App() {
-  return <Directory categories={categories}/>
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path='shop' element={<Shop/>}/>
+        <Route path='sign-in' element={<SignIn/>}/>
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
